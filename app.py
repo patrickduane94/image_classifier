612c34471235037c1f4fb594b2f401295f91aa24
Patrick Duane <patrickdu44@hotmail.com>
2025-05-21 12:51:02 -0500

    Gcloud deployment file added, requirements.txt generated, changes to app.py and template to account for gcloud static file requirements

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import os
 from model import load_model, predict_image
+import base64
+
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = '/tmp'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 model = load_model()
@@ -13,7 +15,7 @@ model = load_model()
 def index():
     prediction = None
     probability = None
-    image_url = None
+    image_data = None
 
     if request.method == 'POST':
         file = request.files['image']
@@ -22,9 +24,11 @@ def index():
             file.save(filepath)
 
             prediction, probability = predict_image(filepath, model)
-            image_url = filepath
 
-    return render_template('index.html', prediction=prediction, image_url=image_url, probability=probability)
+            with open(filepath, "rb") as img_file:
+                image_data = base64.b64encode(img_file.read()).decode('utf-8')
+
+    return render_template('index.html', prediction=prediction, image_data=image_data, probability=probability)
 
 
 if __name__ == '__main__':
